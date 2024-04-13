b276f3e374c4a0b218e6e84fe28480fad2db49bd
luizcalvaosads <155118920+luizcalvaosads@users.noreply.github.com>
2024-04-13 10:45:38 -0300

    Update date.js

--- Dir/utils/date.js
+++ Dir/utils/date.js
@@ -5,11 +5,8 @@ function getDateNow() {
     var dia = dataAtual.getDate();
     var mes = dataAtual.getMonth() + 1; // Os meses começam do zero, então adicionamos 1
     var ano = dataAtual.getFullYear();
-    var hora = dataAtual.getHours();
-    var minutos = dataAtual.getMinutes();
-    var segundos = dataAtual.getSeconds();
 
-    var dataHoraFormatada = dia + '/' + mes + '/' + ano + ' ' + hora + ':' + minutos + ':' + segundos;
+    var dataHoraFormatada = dia + '/' + mes + '/' + ano;
 
     return dataHoraFormatada;
 };
